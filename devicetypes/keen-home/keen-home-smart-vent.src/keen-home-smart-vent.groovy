04eb8a4cab7eead100a9138acabe064c5f50848b
Steven Hoffman <topcornerblast@hotmail.com>
2016-05-11 22:21:47 -0400

    Added Pressure reading to the UI.  Value really means nothing at this point.

--- devicetypes/keen-home/keen-home-smart-vent.src/keen-home-smart-vent.groovy
+++ devicetypes/keen-home/keen-home-smart-vent.src/keen-home-smart-vent.groovy
@@ -54,14 +54,17 @@ metadata {
         valueTile("temperature", "device.temperature", inactiveLabel: false) {
             state "temperature", label:'${currentValue}°',
             backgroundColors:[
-                [value: 31, color: "#153591"],
-                [value: 44, color: "#1e9cbb"],
-                [value: 59, color: "#90d2a7"],
-                [value: 74, color: "#44b621"],
-                [value: 84, color: "#f1d801"],
-                [value: 95, color: "#d04e00"],
-                [value: 96, color: "#bc2323"]
+                	[value: 32, color: "#153591"],
+                    [value: 44, color: "#1e9cbb"],
+                    [value: 59, color: "#90d2a7"],
+					[value: 74, color: "#44b621"],
+					[value: 84, color: "#f1d801"],
+					[value: 92, color: "#d04e00"],
+					[value: 98, color: "#bc2323"]
             ]
+        }
+        valueTile("pressure", "device.pressure", inactiveLabel: false, decoration: "flat") {
+        	state "pressure", label: 'Pressure \n${currentValue} Pa', backgroundColor:"#ffffff"
         }
         valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat") {
             state "battery", label: 'Battery \n${currentValue}%', backgroundColor:"#ffffff"
@@ -70,7 +73,7 @@ metadata {
             state "serial", label:'${currentValue}', backgroundColor:"#ffffff"
         }
         main "switch"
-        details(["switch","refresh","temperature","levelSliderControl","battery"])
+        details(["switch","temperature","levelSliderControl","pressure","battery","refresh"])
     }
 }
 
